304e4ddffbdccd0b63760bbdffd81f4bd338156b
Isaac Truss <IT268@canterbury.ac.uk>
2024-11-28 17:25:50 +0000

    Update to Capabilites
    Added the ability to; copy and move files; create and delete files; and check if the program has permission to do certain actions like read or write to the file.

--- FileHandler.cs
+++ FileHandler.cs
@@ -1,125 +1,33 @@
 ﻿using System.IO;
+using System.Security.Permissions;
 
 namespace fileHanding
 {
-
-    public class FileHandler : IDisposable
+    public class FileHandler
     {
 
-        private class Logging : IDisposable
-        {
-            private string path;
-            private FileStream fs;
-            private StreamWriter sw;
+        private string logPath = "";
 
-            private bool closed = false;
-
-            private bool disposed = false;
-            private object Lock = new object();
-
-            public Logging(string path)
-            {
-                this.path = path;
-                fs = new FileStream(path, FileMode.Append, FileAccess.Write);
-                sw = new StreamWriter(fs);
-            }
-
-            public void log(string line)
-            {
-                if (closed) // Ensures the file is open to edit
-                {
-                    fs = new FileStream(path, FileMode.Append, FileAccess.Write);
-                    sw = new StreamWriter(fs);
-
-                    closed = false;
-                }
-
-                sw.WriteLine(line);
-
-                close();
-            }
-
-            private void close()
-            {
-                sw.Flush();
-                sw.Close();
-                fs.Close();
-
-                closed = true;
-            }
-
-            // used to dispose the prorgam when necessary 
-            public void Dispose()
-            {
-                lock (Lock)
-                {
-                    Dispose(true);
-                    GC.SuppressFinalize(this);
-                }
-            }
-
-            protected virtual void Dispose(bool disposing)
-            {
-                if ((!disposed) && disposing)
-                {
-                    if (closed == false)
-                    {
-                        close();
-                    }
-
-                    fs?.Dispose();
-                    sw?.Dispose();
-
-                    disposed = true;
-                }
-            }
-
-            ~Logging()
-            {
-                Dispose(false);
-            }
-        }
-
-        // Used for disposing the class after it's used
-        private bool disposed = false;
-        private object Lock = new object();
-
-        // Basic information needed to the file
         private string path;
-        private string? setMode = null;
-
-        // Modes used to define access to the file
-        private FileMode fileMode;
-        private FileAccess fileAccess;
-
-        // Used to edit the files depending on the mode
-        private FileStream fs;
-        private StreamReader sr;
-        private StreamWriter sw;
-
-        // Used to ensure the file gets closed
-        private bool closed = false;
-
-        // Defines the program should create the file if it does not exist
+        private string mode;
+        private FileInteractor interactor;
         private bool createIfNotFound;
 
         // Used to store the information about the file being handled
         private Dictionary<string, string> properties;
         private FileInfo fileInfo;
 
-        // Used for logging
-        private Logging logger;
-
-        public FileHandler(string logPath, string path, string mode, bool createIfNotFound = true)
+        public FileHandler(string path, string mode, bool createIfNotFound = true)
         {
-            logger = new Logging(logPath);
-
             this.path = path;
+            this.mode = mode;
             this.createIfNotFound = createIfNotFound;
-            setup(mode);
-            fileInfo = new FileInfo(path);
 
-            properties = new Dictionary<string, string>();
+            this.interactor = new FileInteractor(this.logPath, path, mode, createIfNotFound);
+
+            this.fileInfo = new FileInfo(path);
+
+            this.properties = new Dictionary<string, string>();
 
             updateInfo();
         }
@@ -139,220 +47,85 @@ namespace fileHanding
             return properties;
         }
 
-        private void setup(string parsedMode)
+        public bool isReadable()
         {
-            checkFileExists(); // Ensures there is a file to edit if there is not it makes one
-
-            if (setMode == null)
+            try
             {
-
-                if (parsedMode == "A") // Sets up the append mode
-                {
-                    setMode = "A";
-
-                    fileMode = FileMode.Append;
-                    fileAccess = FileAccess.Write;
-
-                    fs = new FileStream(path, fileMode, fileAccess);
-                    sw = new StreamWriter(fs);
-                }
-
-                else if (parsedMode == "W") // Sets up the write mode
-                {
-                    setMode = "W";
-
-                    sw = new StreamWriter(path);
-                }
-
-                else if (parsedMode == "R") // Sets up the read mode
-                {
-                    setMode = "R";
-
-                    fileMode = FileMode.Open;
-                    fileAccess = FileAccess.Read;
-
-                    fs = new FileStream(path, fileMode, fileAccess);
-                    sr = new StreamReader(fs);
-                }
+                FileIOPermission readCheck = new FileIOPermission(FileIOPermissionAccess.Read, path);
+                readCheck.Demand();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return false;
             }
 
-            else if (setMode != null) // used to change function to a different handler if nexessary but ensures everythin is finsihed up before hand
-            {
-                if (!closed)
-                {
-                    close();
-                }
-
-                setMode = null;
-                setup(parsedMode);
-                closed = false;
-            }
-
-            else
-            {
-                throw new ArgumentException("Invalid Mode");
-            }
+            return true;
         }
 
-        private void checkFileExists() // Ensures a file exists and creates one if the user wishes
+        public bool isWriteable()
         {
-            if (!File.Exists(path))
+            try
             {
-                if (createIfNotFound)
-                {
-                    File.Create(path).Close();
-
-                    logger.log($"{path} created at {DateTime.Now}"); // Logs the file creation
-                }
-
-                else
-                {
-                    throw new FileNotFoundException("File does not exist");
-                }
+                FileIOPermission readCheck = new FileIOPermission(FileIOPermissionAccess.Write, path);
+                readCheck.Demand();
             }
+            catch (UnauthorizedAccessException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool isAppendable()
+        {
+            try
+            {
+                FileIOPermission readCheck = new FileIOPermission(FileIOPermissionAccess.Append, path);
+                readCheck.Demand();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public void append(List<string> lines, bool writeLines = true)
         {
-            if (setMode != "A")
-            {
-                setup("A");
-            }
-
-            if (writeLines)
-            {
-                for (int i = 0; i < lines.Count; i++)
-                {
-                    sw.WriteLine(lines[i]);
-                }
-            }
-
-            else
-            {
-                for (int i = 0; i < lines.Count; i++)
-                {
-                    sw.Write(lines[i]);
-                }
-            }
-
-            close();
-
-            logger.log($"{path} appended to at {DateTime.Now}");
-
-            updateInfo();
+            interactor.append(lines, writeLines);
         }
 
         public void write(List<string> lines, bool writeLines = true)
         {
-            if (setMode != "W")
-            {
-                setup("W");
-            }
-
-            if (writeLines)
-            {
-                for (int i = 0; i < lines.Count; i++)
-                {
-                    sw.WriteLine(lines[i]);
-                }
-            }
-
-            else
-            {
-                for (int i = 0; i < lines.Count; i++)
-                {
-                    sw.Write(lines[i]);
-                }
-            }
-
-            close();
-
-            logger.log($"{path} written to at {DateTime.Now}");
-
-            updateInfo();
+            interactor.write(lines, writeLines);
         }
 
         public string[] read()
         {
-            if (setMode != "R")
-            {
-                setup("R");
-            }
-
-            List<string> lines = new List<string>();
-            sr.BaseStream.Seek(0, SeekOrigin.Begin);
-            string line = sr.ReadLine();
-
-            while (line != null)
-            {
-                lines.Add(line);
-                line = sr.ReadLine();
-            }
-
-            close();
-
-            logger.log($"{path} read at {DateTime.Now}");
-
-            updateInfo();
-
-            return lines.ToArray();
+            return interactor.read();
         }
 
-        public void close() // CLoses the file based off of the mode
+        public void createFile()
         {
-            if (setMode == "A")
-            {
-                sw.Flush();
-                sw.Close();
-                fs.Close();
-            }
-
-            else if (setMode == "W")
-            {
-                sw.Flush();
-                sw.Close();
-            }
-
-            else if (setMode == "R")
-            {
-                sr.Close();
-                fs.Close();
-            }
-
-            setMode = null;
-            closed = true;
+            File.Create(path).Close();
         }
 
-        public void Dispose()
+        public void deleteFile()
         {
-            lock (Lock)
-            {
-                Dispose(true);
-                GC.SuppressFinalize(this);
-            }
+            File.Delete(path);
         }
 
-        protected virtual void Dispose(bool disposing)
+        public void moveFile(string newPath)
         {
-            if ((!disposed) && disposing)
-            {
-                if (closed == false)
-                {
-                    close();
-                }
-
-                fs?.Dispose();
-                sw?.Dispose();
-                sr?.Dispose();
-                logger?.Dispose();
-
-                disposed = true;
-            }
+            File.Move(path, newPath);
+            path = newPath;
         }
 
-        ~FileHandler()
+        public void copyFile(string newPath)
         {
-            Dispose(false);
+            File.Copy(path, newPath);
         }
     }
 }
